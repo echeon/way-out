09c92f7ecc9aaeaef62bf54a5dcca335ba89b169
Euibin Cheon <euibinc@gmail.com>
2017-03-05 21:29:59 -0500

    Update instruction bar style

---
+++ app/components/shared/InstructionBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { colors, globals } from '../../styles';
+
+const styles = StyleSheet.create({
+  instructionBar: {
+    padding: 5,
+    backgroundColor: colors.white,
+    backgroundColor: colors.primaryColorLight,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: colors.dividerColor,
+  },
+  text: {
+    textAlign: 'center',
+  },
+})
+
+const InstructionBar = ({ prompt }) => (
+  <View style={styles.instructionBar}>
+    <Text style={styles.text}>{prompt}</Text>
+  </View>
+);
+
+export default InstructionBar;
